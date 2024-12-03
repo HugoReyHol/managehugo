74b9ad2c65c8852fcb6399afd89666c4c7a7493f
Hugo <hugo.reyhol@educa.jcyl.es>
2024-12-03 13:16:58 +0100

    Campo sprint_id many2one computado

--- models/task.py
+++ models/task.py
@@ -1,3 +1,5 @@
+import datetime
+
 from odoo import models, fields, api
 
 
@@ -13,7 +15,7 @@ class task(models.Model):
     code = fields.Char(string='Código', compute='_get_code')
 
     # Relaciones entre tablas
-    sprint_id = fields.Many2one(comodel_name="managehugo.sprint", string="Sprint", required=True, ondelete="cascade")
+    sprint_id = fields.Many2one(comodel_name="managehugo.sprint", string="Sprint", compute="_get_sprint", store=True)
     technologies_ids = fields.Many2many(comodel_name='managehugo.technology',
                                         string='Technologies',
                                         relation='tasks_technologies',
@@ -29,3 +31,15 @@ class task(models.Model):
     def _get_code(self):
         for task in self:
             task.code = "TSK_" + str(task.id)
+
+    @api.depends('code')
+    def _get_sprint(self):
+        for task in self:
+            sprints = self.env['managehugo.sprint'].search([('project_id.id', '=', task.history_id.project_id.id)])
+            found = False
+            for sprint in sprints:
+                if isinstance(sprint.end_date, datetime.datetime) and sprint.end_date > datetime.datetime.now():
+                    task.sprint_id = sprint.id
+                    found = True
+            if not found:
+                task.sprint_id = False
