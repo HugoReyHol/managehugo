76eed04a0833ea0621b46eefe1073f90ad97242a
Hugo <hugo.reyhol@educa.jcyl.es>
2024-12-16 14:10:51 +0100

    Añadido modulo developer

---
+++ models/developer.py
@@ -0,0 +1,13 @@
+from odoo import fields, models, api
+
+
+class ModelName(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    technologies_ids = fields.Many2many(
+        comodel_name='managehugo.technology',
+        string='Technologies',
+        relation='developer_technologies',
+        column1='developer_id',
+        column2='technologies_ids')
